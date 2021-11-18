5239e406b896e90793ecc80229ee685c93b82554
cstrigl <cstrigl@mail.csuchico.edu>
2021-11-17 23:31:32 -0800

    started front end working on back right now

--- game/models.py
+++ game/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Board(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    location = models.CharField(max_length=4)
+    value = models.CharField(max_length=7)
+    class Meta:
+        unique_together = (("user", "location"))
--- game/views.py
+++ game/views.py
@@ -1,6 +1,42 @@
 from django.shortcuts import render
 
-# Create your views here.
+@login_required(login_url='/login/')
 def game_board(request):
-    context = {}
-    return render(request, 'game/board.html', context)
+
+    if ((Board.objects.filter(user=request.user).count() == 0) or (request.method == 'GET' and 'new_game' in request.GET)):
+        newGame(request)
+
+    page_data = { "rows": [], "chess_form": ChessForm }
+
+    if (request.method == 'POST'):
+        chess_form = ChessForm(request.POST)
+        if (chess_form.is_valid()):
+            location = chess_form.cleaned_data["location"]
+            fromLoc = location[:2]
+            toLoc = location[2:]
+            value = ""
+            try:
+                value = Board.objects.get(user=request.user, location=fromLoc).value
+            except Board.DoesNotExist:
+                value = "&nbsp;"
+
+            Board.objects.filter(user=request.user, location=toLoc).delete();
+            Board.objects.filter(user=request.user, location=fromLoc).delete();
+            Board(user=request.user, location=toLoc, value=value).save()
+            Board(user=request.user, location=fromLoc, value="&nbsp;").save()
+        else:
+            page_data["chess_form"] = chess_form
+
+
+    for row in range (8,0,-1):
+        row_data = {}
+        for col in ['a','b','c','d','e','f','g','h']:
+            id = str(col)+str(row)
+            try:
+                record = Board.objects.get(user=request.user, location=id)
+                row_data[id] = record.value
+            except Board.DoesNotExist:
+                row_data[id] = "&nbsp;"
+        page_data.get("rows").append(row_data)
+
+    return render(request, 'game/board.html', page_data)
